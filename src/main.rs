c53e3a5c57c47b168c3838d40235b38054a31c14
João Paiva <jgpaiva@gmail.com>
2021-12-06 08:49:30 +0000

    Rename to sort correctly

--- src/main.rs
+++ src/main.rs
@@ -12,44 +12,44 @@ fn main() {
 fn days_2021() {
     use year_2021::*;
     println!("2021");
-    println!("day1 part 1: {}", day1::part1(&read_lines("2021/day1")));
-    println!("day1 part 2: {}", day1::part2(&read_lines("2021/day1")));
-    println!("day2 part 1: {}", day2::part1(&read_lines("2021/day2")));
-    println!("day2 part 2: {}", day2::part2(&read_lines("2021/day2")));
-    println!("day3 part 1: {}", day3::part1(&read_lines("2021/day3")));
-    println!("day3 part 2: {}", day3::part2(&read_lines("2021/day3")));
-    println!("day4 part 1: {}", day4::part1(read_file("2021/day4")));
-    println!("day4 part 2: {}", day4::part2(read_file("2021/day4")));
-    println!("day5 part 1: {}", day5::part1(read_file("2021/day5")));
-    println!("day5 part 2: {}", day5::part2(read_file("2021/day5")));
-    println!("day6 part 1: {}", day6::part1(read_file("2021/day6"), 80));
-    println!("day6 part 2: {}", day6::part2(read_file("2021/day6"), 256));
+    println!("day1 part 1: {}", day01::part1(&read_lines("2021/day1")));
+    println!("day1 part 2: {}", day01::part2(&read_lines("2021/day1")));
+    println!("day2 part 1: {}", day02::part1(&read_lines("2021/day2")));
+    println!("day2 part 2: {}", day02::part2(&read_lines("2021/day2")));
+    println!("day3 part 1: {}", day03::part1(&read_lines("2021/day3")));
+    println!("day3 part 2: {}", day03::part2(&read_lines("2021/day3")));
+    println!("day4 part 1: {}", day04::part1(read_file("2021/day4")));
+    println!("day4 part 2: {}", day04::part2(read_file("2021/day4")));
+    println!("day5 part 1: {}", day05::part1(read_file("2021/day5")));
+    println!("day5 part 2: {}", day05::part2(read_file("2021/day5")));
+    println!("day6 part 1: {}", day06::part1(read_file("2021/day6"), 80));
+    println!("day6 part 2: {}", day06::part2(read_file("2021/day6"), 256));
 }
 
 fn days_2018() {
     use year_2018::*;
     println!("2018");
-    println!("day1: {}", day1::part2(read_lines("2018/day1")));
-    println!("day2 part 1: {}", day2::part1(read_lines("2018/day2")));
-    println!("day2 part 2: {}", day2::part2(read_lines("2018/day2")));
-    println!("day3: {}", day3::day3(read_lines("2018/day3")));
-    println!("day4: {}", day4::day4(read_lines("2018/day4")));
-    println!("day5: {}", day5::day5(read_lines("2018/day5")));
+    println!("day1: {}", day01::part2(read_lines("2018/day1")));
+    println!("day2 part 1: {}", day02::part1(read_lines("2018/day2")));
+    println!("day2 part 2: {}", day02::part2(read_lines("2018/day2")));
+    println!("day3: {}", day03::day3(read_lines("2018/day3")));
+    println!("day4: {}", day04::day4(read_lines("2018/day4")));
+    println!("day5: {}", day05::day5(read_lines("2018/day5")));
     // skipped because it takes a while
     //    println!("day6 part 1: {}", day6::part1(read_lines("2018/day6")));
     println!(
         "day6 part 2: {}",
-        day6::part2(read_lines("2018/day6"), 10000)
+        day06::part2(read_lines("2018/day6"), 10000)
     );
-    println!("day7 part 1: {}", day7::part1(&read_lines("2018/day7")));
+    println!("day7 part 1: {}", day07::part1(&read_lines("2018/day7")));
     println!(
         "day7 part 2: {}",
-        day7::part2(&read_lines("2018/day7"), 5, 60)
+        day07::part2(&read_lines("2018/day7"), 5, 60)
     );
-    println!("day8 part 1: {}", day8::part1(&read_lines("2018/day8")));
-    println!("day8 part 2: {}", day8::part2(&read_lines("2018/day8")));
+    println!("day8 part 1: {}", day08::part1(&read_lines("2018/day8")));
+    println!("day8 part 2: {}", day08::part2(&read_lines("2018/day8")));
     // part 2 is the same as part 1, but faster
-    println!("day9 part 1: {}", day9::part2(473, 70904));
+    println!("day9 part 1: {}", day09::part2(473, 70904));
     // skipped because it takes a while
     // println!("day9 part 2: {}", day9::part2(473, 7090400));
     println!(
