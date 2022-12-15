418242932efcceee33d051d506198449c3054d26
João Paiva <jgpaiva@gmail.com>
2022-12-15 10:25:41 +0000

    Add 2022 day 14 part 2

--- src/year_2022/day14.rs
+++ src/year_2022/day14.rs
@@ -1,4 +1,3 @@
-use anyhow::{anyhow, Context};
 use itertools::Itertools;
 
 #[cfg(test)]
@@ -8,52 +7,90 @@ use crate::utils;
 fn test() {
     let input = utils::read_test_file(file!());
     assert_eq!(part1(&input), 24);
-    //assert_eq!(part2(&input), 140);
+    assert_eq!(part2(&input), 93);
 }
 
-pub fn part2(lines: &str) -> usize {
-    todo!()
+pub fn part2(lines: &str) -> i32 {
+    let input = parse_input(lines);
+    let map = create_map(input, true);
+    let (map, result) = run_sim(map);
+    let y_max = map.y_max;
+    let x_max = map.x_max;
+    let x_min = map.x_min;
+    let right_hand_side: i32 = (0..=(y_max as i32 - (x_max as i32 - 500))).sum();
+    let left_hand_side: i32 = (0..=(y_max as i32 - (500 - x_min as i32))).sum();
+
+    #[cfg(test)]
+    {
+        println!("{}", map.to_string(),);
+        dbg!(&result);
+        dbg!(&right_hand_side);
+        dbg!(&left_hand_side);
+        dbg!(&x_min);
+        dbg!(&x_max);
+        dbg!(&y_max);
+    }
+
+    result as i32 + right_hand_side + left_hand_side
 }
 
 #[allow(clippy::needless_range_loop)]
 pub fn part1(lines: &str) -> usize {
     let input = parse_input(lines);
-    let mut map = create_map(input);
+    let map = create_map(input, false);
+    let (_map, result) = run_sim(map);
+    result
+}
+
+fn run_sim(mut map: Map) -> (Map, usize) {
     let start_point = (500, 0);
     *map.at(start_point) = El::Sand;
-
-    for i in 0..10000 {
-        let mut current_point = (start_point.0, start_point.1 + 1);
+    for i in 0..100000 {
+        let mut current_point = start_point;
         loop {
-            let next_point = (current_point.0, current_point.1 + 1);
             if current_point.1 == map.y_max {
-                return i;
+                return (map, i);
             }
-            if let El::Air = map.at(next_point) {
-                *map.at(next_point) = El::CurrentSand;
-                *map.at(current_point) = El::Air;
-                current_point = next_point;
-            } else {
-                let next_point = (current_point.0 - 1, current_point.1 + 1);
-                if let El::Air = map.at(next_point) {
-                    *map.at(next_point) = El::CurrentSand;
-                    *map.at(current_point) = El::Air;
-                    current_point = next_point;
-                } else {
-                    let next_point = (current_point.0 + 1, current_point.1 + 1);
-                    if let El::Air = map.at(next_point) {
-                        *map.at(next_point) = El::CurrentSand;
-                        *map.at(current_point) = El::Air;
-                        current_point = next_point;
-                    } else {
-                        *map.at(current_point) = El::Sand;
-                        break;
-                    }
-                }
+            if try_move(
+                &mut map,
+                (current_point.0, current_point.1 + 1),
+                &mut current_point,
+            ) {
+                continue;
             }
+            if try_move(
+                &mut map,
+                (current_point.0 - 1, current_point.1 + 1),
+                &mut current_point,
+            ) {
+                continue;
+            }
+            if try_move(
+                &mut map,
+                (current_point.0 + 1, current_point.1 + 1),
+                &mut current_point,
+            ) {
+                continue;
+            }
+            if current_point == start_point {
+                return (map, i + 1);
+            }
+            *map.at(current_point) = El::Sand;
+            break;
         }
     }
     unreachable!("reached maximum iterations with no result");
+}
+
+fn try_move(map: &mut Map, next_point: (usize, usize), current_point: &mut (usize, usize)) -> bool {
+    if let El::Air = map.at(next_point) {
+        *map.at(next_point) = El::CurrentSand;
+        *map.at(*current_point) = El::Air;
+        *current_point = next_point;
+        true
+    } else {
+        false
+    }
 }
 
 fn parse_input(lines: &str) -> Vec<Vec<(usize, usize)>> {
@@ -72,12 +109,15 @@ fn parse_input(lines: &str) -> Vec<Vec<(usize, usize)>> {
         .collect()
 }
 
-fn create_map(input: Vec<Vec<(usize, usize)>>) -> Map {
+fn create_map(mut input: Vec<Vec<(usize, usize)>>, surrounded: bool) -> Map {
     let all_points: Vec<(usize, usize)> = input.iter().flat_map(|x| x.iter().copied()).collect();
     let x_max = all_points.iter().map(|x| x.0).max().unwrap() + 2;
     let x_min = all_points.iter().map(|x| x.0).min().unwrap() - 2;
-    let y_max = all_points.iter().map(|x| x.1).max().unwrap() + 1;
+    let y_max = all_points.iter().map(|x| x.1).max().unwrap() + 2;
     let mut map = Map::new(x_min, x_max, y_max);
+    if surrounded {
+        input.push(vec![(x_min, 0), (x_min, y_max), (x_max, y_max), (x_max, 0)])
+    }
     for line in input {
         for (start, end) in line.iter().zip(line.iter().skip(1)) {
             if start.0 == end.0 {
@@ -101,6 +141,7 @@ fn create_map(input: Vec<Vec<(usize, usize)>>) -> Map {
 struct Map {
     m: Vec<Vec<El>>,
     x_min: usize,
+    x_max: usize,
     y_max: usize,
 }
 
@@ -112,6 +153,7 @@ impl Map {
         Self {
             m: map,
             x_min,
+            x_max,
             y_max,
         }
     }
