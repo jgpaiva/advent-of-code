ea36760dce05565b9f41d55b8e52fb9ce35772d4
João Paiva <jgpaiva@gmail.com>
2022-12-16 00:25:59 +0000

    Add 2022 day 15 part 1 working fast

--- src/year_2022/day15.rs
+++ src/year_2022/day15.rs
@@ -1,9 +1,8 @@
-use std::collections::HashSet;
-
 #[cfg(test)]
 use crate::utils;
 
 use anyhow::Context;
+use itertools::Itertools;
 use regex::Regex;
 
 #[test]
@@ -18,35 +17,66 @@ pub fn part2(lines: &str) -> i32 {
     0
 }
 
-pub fn part1(lines: &str) -> usize {
+pub fn part1(lines: &str) -> i32 {
     let line = 2000000;
     calculate_line(lines, line)
 }
 
-fn calculate_line(lines: &str, line: i32) -> usize {
+fn calculate_line(lines: &str, line: i32) -> i32 {
     let input = parse_input(lines).unwrap();
-    let mut affected_positions = HashSet::<i32>::new();
+    let mut affected_ranges = vec![];
     for (sensor, beacon) in input.iter() {
         let d_beacon = (sensor.0 - beacon.0).abs() + (sensor.1 - beacon.1).abs();
-        // can affect the line if distance from line to sensor is < than sensor to beacon
+        // can affect the line if distance from line to sensor is <= than sensor to beacon
         let d_line = (sensor.1 - line).abs();
         if d_line <= d_beacon {
-            for i in sensor.0 - (d_beacon - d_line)..=sensor.0 + (d_beacon - d_line) {
-                affected_positions.insert(i);
-            }
+            affected_ranges.push((
+                sensor.0 - (d_beacon - d_line),
+                sensor.0 + (d_beacon - d_line),
+            ));
         }
     }
-    for (_, beacon) in input {
-        if beacon.1 == line {
-            affected_positions.remove(&beacon.0);
-        }
-    }
-    affected_positions.len()
+    affected_ranges.sort_by(|a, b| a.0.cmp(&b.0));
+    let affected_ranges =
+        affected_ranges
+            .into_iter()
+            .fold(Vec::<(i32, i32)>::new(), |mut acc, v| {
+                if let Some(current) = acc.pop() {
+                    if v.0 <= current.1 + 1 {
+                        acc.push((current.0, v.1.max(current.1)));
+                        acc
+                    } else {
+                        acc.push(v);
+                        acc
+                    }
+                } else {
+                    acc.push(v);
+                    acc
+                }
+            });
+
+    let mut beacons_in_line: Vec<_> = input
+        .iter()
+        .map(|(_, b)| *b)
+        .filter(|b| b.1 == line)
+        .collect();
+    beacons_in_line.sort_by(|a, b| a.1.cmp(&b.1));
+    affected_ranges
+        .into_iter()
+        .map(|r| {
+            let b = beacons_in_line
+                .iter()
+                .filter(|b| b.1 >= r.0 || b.1 <= r.1)
+                .unique_by(|b| (b.0, b.1))
+                .count();
+            r.1 - r.0 + 1 - b as i32
+        })
+        .sum()
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Sensor(i32, i32);
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Beacon(i32, i32);
 
 fn parse_input(lines: &str) -> anyhow::Result<Vec<(Sensor, Beacon)>> {
